1f0c4b84ff8e5cc9ec15b62a6430cba27622a3dc
Rahfay <saberirahfay@gmail.com>
2026-05-03 10:03:38 -0400

    Add configuration file with directory paths for data, vector database, memory, and logs

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+ROOT = os.getcwd()
+
+DATA_DIR = os.path.join(ROOT,"data")
+VECTOR_DB_DIR = os.path.join(DATA_DIR,"vector_db")
+
+PATH_TO_CHAT_MEMORY = os.path.join(ROOT,"memory")
+LOGS_DIR = os.path.join(ROOT,"logs")

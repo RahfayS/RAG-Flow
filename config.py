35f60713bf44e2390d6abe4082a57b8c01162298
Rahfay <saberirahfay@gmail.com>
2026-05-03 18:14:51 -0400

    Add session directory

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
 import os
 
 ROOT = os.getcwd()
+SESSIONS_DIR = os.path.join(ROOT,"sessions")
 
 DATA_DIR = os.path.join(ROOT,"data")
+SESSIONS_DIR = os.path.join(DATA_DIR,"sessions")
+
 VECTOR_DB_DIR = os.path.join(DATA_DIR,"vector_db")
 
 PATH_TO_CHAT_MEMORY = os.path.join(ROOT,"memory")

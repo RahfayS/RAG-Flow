c55c80ad5f241eef8da92e3c523ebcd59e51163c
Rahfay <saberirahfay@gmail.com>
2026-05-09 14:10:30 -0400

    Add path to database

--- app/config.py
+++ app/config.py
@@ -8,5 +8,5 @@ SESSIONS_DIR = os.path.join(DATA_DIR,"sessions")
 
 VECTOR_DB_DIR = os.path.join(DATA_DIR,"vector_db")
 
-PATH_TO_CHAT_MEMORY = os.path.join(ROOT,"memory")
+PATH_TO_DATABASE = os.path.join(ROOT,"database")
 LOGS_DIR = os.path.join(ROOT,"logs")
